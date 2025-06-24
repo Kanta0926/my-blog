063450642a150792865ad95d70b29b22dabf6d97
kanta <kanta.sato0926@gmail.com>
2025-06-24 23:40:43 +0900

    axios追加

---
+++ pages/blogList.vue
@@ -0,0 +1,4 @@
+// BlogList.vue など import axios from 'axios'; export default { data() { return
+{ posts: [], }; }, async created() { const res = await
+axios.get('https://yourdomain.com/wp-json/wp/v2/posts'); this.posts = res.data;
+}, };
